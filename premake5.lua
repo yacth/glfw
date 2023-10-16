8f39f11f66b2a287f738595e46cf46b062312b25
Yacine <yacine@c3rios.com>
2023-10-15 20:33:52 -0400

    Update premake file

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,7 @@
 project "GLFW"
     kind "StaticLib"
     language "C"
+    staticruntime "on"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -24,9 +25,7 @@ project "GLFW"
     }
 
 	filter "system:windows"
-    buildoptions { "-std=c11", "-lgdi32" }
     systemversion "latest"
-    staticruntime "On"
 
     files
     {
@@ -47,5 +46,12 @@ project "GLFW"
       "_GLFW_WIN32",
       "_CRT_SECURE_NO_WARNINGS"
 		}
-    filter { "system:windows", "configurations:Release" }
-    buildoptions "/MT"
+
+  filter "configurations:Debug"
+    runtime "Debug"
+    symbols "on"
+
+  filter "configurations:Release"
+    runtime "Release"
+    optimize "on"
+
